187185eaaa10aa6d0399a1dcda33aa21ab754d03
Jeffrey Huang <jeffreyhuang@propertyme.com>
2023-02-22 14:01:52 +1100

    consider csv file with no header row

--- fix-csv.js
+++ fix-csv.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
 try {
-  const data = fs.readFileSync('./NZ_Addresses.csv', 'utf8');
+  const data = fs.readFileSync('./NZ_Addresses_prod.csv', 'utf8');
   const strung = data.toString();
   const allLines = strung.split(/\n/g);
-  if (allLines.length < 2) {
+  const isHeaderRow = allLines[0].startsWith('Address');
+
+  if (allLines.length < (isHeaderRow ? 2 : 1)) {
     console.log('only one line exists. exiting early with assumption that one line is the heading.');
     return;
   }
 
-  const lines = allLines.slice(1); // exclude heading row
+  const lines = isHeaderRow ? allLines.slice(1) : allLines; // exclude heading row
 
   const combinedLines = [];
   let pair = [];
@@ -27,7 +29,7 @@ try {
   }
 
   const writeStream = fs.createWriteStream('./NZ_Addresses_fixed.csv');
-  writeStream.write(`${allLines[0]}\n`);
+  writeStream.write(`Address\n`);
 
   for (const line of combinedLines) {
       writeStream.write(`${line}\n`);
